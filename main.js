a8242969a6ee7872ea4575754ab1f491d5fc2e42
VioletViolaVi <vivian.v18723@gmail.com>
2020-05-30 13:59:06 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const baseURL = "https://swapi.dev/api/";
+
+function getData(type, cb) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
+}
+
+function getTableHeaders(obj){
+    var tableHeaders = [];
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push( `<td>${key}</td>`)
+    })
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+  getData(type, function(data) {
+    data = data.results;
+    var tableHeaders = getTableHeaders(data[0]);
+
+    data.forEach(function(item) {
+        var dataRow = [];
+        Object.keys(item).forEach(function(key){
+            var rowData = item[key].toString();
+            var truncatedData = rowData.substring(0,15);
+            dataRow.push (`<td>${item[key]}</td>`)
+        });
+        tableRows.push(`<tr>${dataRow}</tr>`);
+    //   el.innerHTML += "<p>" + item.name + "</p>";
+    });
+
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+  });
+}
